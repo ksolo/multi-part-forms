fa8b50f0a8373816c31e492ca9a217a25e8e8dd9
Kevin Solorio <soloriok@gmail.com>
2013-07-24 10:15:20 -0500

    set app root

--- config/environment.rb
+++ config/environment.rb
@@ -22,8 +22,9 @@ require 'erb'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-
 APP_NAME = APP_ROOT.basename.to_s
+
+set :root, APP_ROOT
 
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
