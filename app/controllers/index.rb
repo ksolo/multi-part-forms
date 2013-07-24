a0cb5592db1f7a22761bbf171b1952b773ba7c26
Kevin Solorio <soloriok@gmail.com>
2013-07-24 10:42:13 -0500

    quick pass on controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
+
+get '/new' do
+  erb :new
+end
+
+post '/' do
+  user = User.new(params[:user])
+  if user.save
+    redirect to '/'
+  else
+    erb :new
+  end
+end
+
